94cf5a331a7001892b00abf6e99302f3a3e0693b
Yazmin-Silva <135621372+Yazmin-Silva@users.noreply.github.com>
2023-06-23 12:47:20 -0400

    Update models.py
    codigo models

--- mate_error_app/models.py
+++ mate_error_app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    rut = models.CharField(max_length=12)
+    curso = models.CharField(max_length=50)
+    correo = models.EmailField()
+
+
+class Pregunta(models.Model):
+    enunciado = models.TextField()
+    idPregunta = models.IntegerField()
+    respuesta_correcta = models.CharField()
+    siguiente_pregunta_correcta = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='preguntas_correctas'
+    )
+    siguiente_pregunta_incorrecta = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='preguntas_incorrectas'
+    )
+
+
+class Respuesta(models.Model):
+    idRespuesta = models.IntegerField()
+    pregunta = models.ForeignKey(
+        Pregunta,
+        on_delete=models.CASCADE,
+        related_name='respuestas'
+    )
+    alumno = models.ForeignKey(
+        Alumno,
+        on_delete=models.CASCADE,
+        related_name='respuestas'
+    )
+    respuesta = models.CharField(max_length=255)
+
 # Create your models here.
